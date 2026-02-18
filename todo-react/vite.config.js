4e9719c0a92dc578edfd5aae1d2050ba261786b5
Vsevolod Bokov <vsevolod.bokov@bellevuecollege.edu>
2026-02-17 16:31:16 -0800

    modded vite.config.js to work with port 3000

--- todo-react/vite.config.js
+++ todo-react/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/register': 'http://localhost:3000',
+      '/login': 'http://localhost:3000',
+      '/logout': 'http://localhost:3000',
+      '/todos': 'http://localhost:3000',
+    }
+  }
 })
